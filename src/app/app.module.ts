665d5bcff3c743f3677fb7d7c9318c9367c8046e
Edgard Alvarado <Edgard-0211@hotmail.com>
2022-06-01 22:45:03 -0500

    se empieza con la pantallaza de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutsComponent } from './components/produts/produts/produts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutsComponent
   ],
   imports: [
     BrowserModule,
