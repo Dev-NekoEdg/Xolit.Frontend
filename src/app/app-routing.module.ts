0e732a5194e26b704b3d5b06d20aa9c41af42c94
Edgard Alvarado <Edgard-0211@hotmail.com>
2022-06-02 21:41:07 -0500

    se agrega bootstrap y bootstrap-icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { ProdutsComponent } from './components/produts/produts/produts.component';
 
 const routes: Routes = [
-  {path:"products", component: ProdutsComponent, pathMatch: "full"}
+  {path:"products", component: ProdutsComponent, pathMatch: "full"},
+  {path:"home", component: HomeComponent, pathMatch: "full"}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutsComponent } from './components/produts/produts/produts.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProdutsComponent
+    ProdutsComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
